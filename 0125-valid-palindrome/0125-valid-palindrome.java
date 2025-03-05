484b7c4fef94c2844c79732212ab8b4f84b362c2
adipatil-13 <121687565+adipatil-13@users.noreply.github.com>
2025-03-06 00:23:35 +0530

    Time: 15 ms (23.49%), Space: 50.7 MB (5.04%) - LeetHub

--- 0125-valid-palindrome/0125-valid-palindrome.java
+++ 0125-valid-palindrome/0125-valid-palindrome.java
@@ -1,25 +1,14 @@
 class Solution {
     public boolean isPalindrome(String s) {
-        int l = 0, r = s.length() - 1;
-        while(l < r){
-            while(l < r && !validChar(s.charAt(l))){
-                l++;
-            }
-            while(l < r && !validChar(s.charAt(r))){
-                r--;
-            }
-            if(Character.toLowerCase(s.charAt(l)) != Character.toLowerCase(s.charAt(r))){
-                return false;
-            }
-            l++;
-            r--;
-        }
-        return true;
+        s = s.replaceAll("[^a-zA-Z0-9]", "").toLowerCase();
+        return isPalindromeRecursive(s, 0);
     }
-    private boolean validChar(char ch){
-        if((ch >= 'A' && ch <= 'Z') || (ch >= 'a' && ch <= 'z') || (ch >= '0' && ch <= '9')){
-            return true;
-        }
-        return false;
+    private boolean isPalindromeRecursive(String s, int i) {
+        // Base case: if i is greater than or equal to the halfway point, it's a palindrome
+        if (i >= s.length() / 2) return true;
+        
+        if (s.charAt(i) != s.charAt(s.length() - i - 1)) return false;
+        
+        return isPalindromeRecursive(s, i + 1);
     }
 }
