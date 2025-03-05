58e5cf4b24e8c5e977d5b59d61d342f081e051b8
adipatil-13 <121687565+adipatil-13@users.noreply.github.com>
2025-03-06 01:22:16 +0530

    Update 0509-fibonacci-number-recursion.java

--- 0509-fibonacci-number/0509-fibonacci-number-recursion.java
+++ 0509-fibonacci-number/0509-fibonacci-number-recursion.java
@@ -1,13 +1,6 @@
 class Solution {
     public int fib(int n) {
-        int n1 = 0, n2 = 1;
         if(n == 0 || n == 1) return n;
-
-        for(int i = 2; i <= n; i++){
-            int temp = n2;
-            n2 = n1 + n2;
-            n1 = temp;
-        }
-        return n2;
+        return fib(n - 1) + fib(n - 2);
     }
 }
