8d3d6b7b35a578fa7e7d091fd5ea21766c6ed4a3
Aditya Patil <121687565+adipatil-13@users.noreply.github.com>
2025-03-09 03:06:27 +0530

    Update 0042-trapping-rain-water.java

--- 0042-trapping-rain-water/0042-trapping-rain-water.java
+++ 0042-trapping-rain-water/0042-trapping-rain-water.java
@@ -1,10 +1,11 @@
 class Solution {
     public int trap(int[] height) {
-        int l=0, r = height.length-1;
+        //O(n)TC O(1)SC
+        int l = 0, r = height.length - 1;
         int leftMax = height[l], rightMax = height[r];
-        int res=0;
+        int res = 0;
 
-        while(l<r){
+        while(l < r){
             if(leftMax < rightMax){
                 l++;
                 leftMax = Math.max(leftMax, height[l]);
