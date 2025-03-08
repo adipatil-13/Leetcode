5ddc9b794e44dc1d3241cef0239b51aafe199ddc
Aditya Patil <121687565+adipatil-13@users.noreply.github.com>
2025-03-09 00:10:00 +0530

    Update 0349-intersection-of-two-arrays.java

--- 0349-intersection-of-two-arrays/0349-intersection-of-two-arrays.java
+++ 0349-intersection-of-two-arrays/0349-intersection-of-two-arrays.java
@@ -1,21 +1,42 @@
 class Solution {
     public int[] intersection(int[] nums1, int[] nums2) {
-        HashSet<Integer> set = new HashSet<>();
-        HashSet<Integer> intersection = new HashSet<>();
-
-        for(int i = 0; i < nums1.length; i++){
-            set.add(nums1[i]);
+        int[] freq = new int[1001];
+        List<Integer> list = new ArrayList<>();
+        for(int num : nums1){
+            freq[num]++;
         }
         for(int num : nums2){
-            if(set.contains(num)){
-                intersection.add(num);
+            if(freq[num] > 0){
+                list.add(num);
+                freq[num] = 0;
             }
         }
-        int[] res = new int[intersection.size()];
-        int i = 0;
-        for(int num : intersection){
-            res[i++] = num;
+        int[] res = new int[list.size()];
+        for (int i = 0; i < list.size(); i++) {
+            res[i] = list.get(i);
         }
+
         return res;
     }
 }
+
+/*public int[] intersection(int[] nums1, int[] nums2) {
+        HashSet<Integer> set = new HashSet<>();
+        HashSet<Integer> intersection = new HashSet<>();
+
+        for(int num : nums1){
+            set.add(num);
+        }
+        for(int n : nums2){
+            if(set.contains(n)){
+                intersection.add(n);
+            }
+        }
+        int[] sol = new int[intersection.size()];
+        int i = 0;
+        for(int num : intersection){
+            sol[i++] = num;
+        }
+        return sol;
+    }
+*/
