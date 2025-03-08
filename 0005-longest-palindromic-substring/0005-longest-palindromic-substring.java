5c26c3e10d90c1b1cfacbe840edf2b8d8d62ec0f
Aditya Patil <121687565+adipatil-13@users.noreply.github.com>
2025-03-09 01:25:03 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

---
+++ 0005-longest-palindromic-substring/0005-longest-palindromic-substring.java
@@ -0,0 +1,12 @@
+class Solution {
+    public String longestPalindrome(String s) {
+        if(s == null || s.length() < 1){
+            return "";
+        }
+        int start = 0, end = 0;
+
+        for(int i = 0; i < s.length(); i++){
+
+        }
+    }
+}
