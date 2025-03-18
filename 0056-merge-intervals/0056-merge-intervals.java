99f59e02375bb4479df3e7cdc01db572a81689de
Aditya Patil <121687565+adipatil-13@users.noreply.github.com>
2025-03-18 20:14:21 +0530

    Update 0056-merge-intervals.java

--- 0056-merge-intervals/0056-merge-intervals.java
+++ 0056-merge-intervals/0056-merge-intervals.java
@@ -1,3 +1,4 @@
+// Time Complexity: O(n log n), Space Complexity: O(n) beats 24.14%
 class Solution {
     public int[][] merge(int[][] intervals) {
         int n = intervals.length;
@@ -13,5 +14,22 @@ class Solution {
             }
         }
         return res.toArray(new int[res.size()][]);
+    }
+}
+
+// Time Complexity: O(n log n), Space Complexity: O(n) beats 60%
+class Solution {
+    public int[][] merge(int[][] intervals) {
+        int count = 0;
+        Arrays.sort(intervals, (a, b) -> Integer.compare(a[0], b[0]));
+        for(int i = 1; i < intervals.length; i++) {
+            if(intervals[count][1] >= intervals[i][0]) {
+                intervals[count][1] = Math.max(intervals[count][1], intervals[i][1]);
+            } else {
+                count++;
+                intervals[count] = intervals[i];
+            }
+        }
+        return Arrays.copyOfRange(intervals, 0, count + 1);
     }
 }
