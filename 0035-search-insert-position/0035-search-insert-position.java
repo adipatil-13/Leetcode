992eb5df3277c00495d34c08f99eb9d8440ea940
Aditya Patil <121687565+adipatil-13@users.noreply.github.com>
2025-03-20 01:47:19 +0530

    Time: 0 ms (100%), Space: 43.3 MB (18.79%) - LeetHub

---
+++ 0035-search-insert-position/0035-search-insert-position.java
@@ -0,0 +1,21 @@
+class Solution {
+    public int searchInsert(int[] nums, int target) {
+        int left = 0, right = nums.length - 1;
+
+        while (left <= right) {
+            int mid = (left + right) / 2;
+
+            if(nums[mid] == target){
+                return mid;
+            } else if (nums[mid] < target) {
+                left = mid + 1;
+            } else {
+                right = mid - 1;
+            }
+        }
+        return left;
+    }
+}
+/*why left? If the loop terminates without finding the target, 
+it means the left pointer has moved to the position where the target should be inserted 
+to maintain the sorted order of the array.*/
