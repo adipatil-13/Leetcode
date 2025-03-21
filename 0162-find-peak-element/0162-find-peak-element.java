54221842c8a9b457e6578f55ed190f8de5af1693
Aditya Patil <121687565+adipatil-13@users.noreply.github.com>
2025-03-21 20:47:19 +0530

    Update 0162-find-peak-element.java

--- 0162-find-peak-element/0162-find-peak-element.java
+++ 0162-find-peak-element/0162-find-peak-element.java
@@ -13,3 +13,24 @@ class Solution {
         return left;
     }
 }
+
+/* public static int findPeakElement(ArrayList<Integer> arr) {
+        int n = arr.size();
+        // Edge cases:
+        if (n == 1) return 0;
+        if (arr.get(0) > arr.get(1)) return 0;
+        if (arr.get(n - 1) > arr.get(n - 2)) return n - 1;
+
+        int low = 1, high = n - 2;
+        while (low <= high) {
+            int mid = (low + high) / 2;
+
+            if (arr.get(mid - 1) < arr.get(mid) && arr.get(mid) > arr.get(mid + 1))
+                return mid;
+
+            if (arr.get(mid) > arr.get(mid - 1)) low = mid + 1;
+            else high = mid - 1;
+        }
+        return -1;
+    }
+*/
