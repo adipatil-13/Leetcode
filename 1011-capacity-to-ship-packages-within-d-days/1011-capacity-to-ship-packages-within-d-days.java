311c01dc10de8f3c34d688e7b6fa6c8e80ecac95
Aditya Patil <121687565+adipatil-13@users.noreply.github.com>
2025-03-22 23:20:12 +0530

    Time: 10 ms (93.5%), Space: 47.6 MB (29.01%) - LeetHub

--- 1011-capacity-to-ship-packages-within-d-days/1011-capacity-to-ship-packages-within-d-days.java
+++ 1011-capacity-to-ship-packages-within-d-days/1011-capacity-to-ship-packages-within-d-days.java
@@ -1,32 +1,32 @@
 class Solution {
     public int shipWithinDays(int[] weights, int days) {
-        int low = Integer.MIN_VALUE, high = 0;
-        for (int i = 0; i < weights.length; i++){
-            high += weights[i];
-            low = Math.max(low, weights[i]);
-        }
-        while (low <= high) {
-            int mid = low + (high - low) / 2;
-            int numberOfDays = noOfDays(weights, mid);
-            if (numberOfDays <= days) {
-                high = mid - 1;
-            } else {
-                low = mid + 1;
-            }
-        }
-        return low;
-    }
-    private int noOfDays(int[] weights, int capacity) {
-        int days = 1, load = 0;
+        int max = -1, total = 0;
 
-        for (int i = 0; i < weights.length; i++) {
-            if (load + weights[i] > capacity) {
-                days++;
-                load = weights[i];
+        for (int weight : weights) {
+            max = Math.max(max, weight);
+            total += weight;
+        }
+
+        int left = max, right = total;
+
+        while (left < right) {
+            int mid = left + (right - left) / 2;
+            int numberOfDays = 1, load = 0;
+
+            for (int weight : weights) {
+                if (load + weight > mid) {
+                    numberOfDays++;
+                    load = 0;
+                }
+                load += weight;
+            }
+
+            if (numberOfDays > days) {
+                left = mid + 1;
             } else {
-                load += weights[i];
+                right = mid;
             }
         }
-        return days;
+        return left;
     }
 }
