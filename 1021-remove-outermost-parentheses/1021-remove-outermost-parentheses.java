b2ba25eaa637f9345cbc8c511f7ce97753d6030e
Aditya Patil <121687565+adipatil-13@users.noreply.github.com>
2025-03-25 00:10:47 +0530

    Time: 2 ms (99.14%), Space: 42.7 MB (25.4%) - LeetHub

---
+++ 1021-remove-outermost-parentheses/1021-remove-outermost-parentheses.java
@@ -0,0 +1,11 @@
+class Solution {
+    public String removeOuterParentheses(String s) {
+        StringBuilder sb = new StringBuilder();
+        int open = 0;
+        for (char ch : s.toCharArray()) {
+            if (ch == '(' && open++ > 0) sb.append(ch);
+            if (ch == ')' && open-- > 1) sb.append(ch);
+        }
+        return sb.toString();
+    }
+}
