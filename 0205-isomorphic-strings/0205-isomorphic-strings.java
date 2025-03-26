afa475a183349b4c3b8de244790936f8c947fb21
Aditya Patil <121687565+adipatil-13@users.noreply.github.com>
2025-03-27 00:12:30 +0530

    Update 0205-isomorphic-strings.java

--- 0205-isomorphic-strings/0205-isomorphic-strings.java
+++ 0205-isomorphic-strings/0205-isomorphic-strings.java
@@ -1,3 +1,5 @@
+// 13ms
+
 class Solution {
     public boolean isIsomorphic(String s, String t) {
         Map<Character, Character> map = new HashMap<>();
@@ -14,6 +16,28 @@ class Solution {
             }
 
             map.put(sChar, tChar);
+        }
+        return true;
+    }
+}
+
+// 8 ms
+
+class Solution {
+    public boolean isIsomorphic(String s, String t) {
+        int[] charS = new int[200];
+        int[] charT = new int[200]; 
+        
+        if(s.length() != t.length()) {
+            return false;
+        }
+        
+        for(int i = 0; i < s.length(); i++) {
+            if(charS[s.charAt(i)] != charT[t.charAt(i)]) {
+                return false; 
+            }
+            charS[s.charAt(i)] = i + 1;
+            charT[t.charAt(i)] = i + 1; 
         }
         return true;
     }
