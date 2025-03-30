19bc1aefa451fc960800f037223ca8fbd5d7e42d
Aditya Patil <121687565+adipatil-13@users.noreply.github.com>
2025-03-31 01:54:49 +0530

    Update 0151-reverse-words-in-a-string.java

--- 0151-reverse-words-in-a-string/0151-reverse-words-in-a-string.java
+++ 0151-reverse-words-in-a-string/0151-reverse-words-in-a-string.java
@@ -15,3 +15,26 @@ class Solution {
         return sb.toString();
     }
 }
+
+/*
+        String[] words = s.split(" ");
+        int left = 0, right = words.length - 1;
+
+        while(left < right){
+            String temp = words[left];
+            words[left] = words[right];
+            words[right] = temp;
+            left++;
+            right--;
+        }
+        StringBuilder res = new StringBuilder();
+        for(String word : words){
+            if(!word.isEmpty()){
+                if(res.length() > 0){
+                    res.append(" ");
+                }
+                res.append(word);
+            }
+        }
+        return res.toString();
+*/
