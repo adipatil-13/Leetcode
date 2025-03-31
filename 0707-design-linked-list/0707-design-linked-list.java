fe7ae2939f55220b49126526e017dd516ad8a487
Aditya Patil <121687565+adipatil-13@users.noreply.github.com>
2025-04-01 01:36:54 +0530

    Update 0707-design-linked-list.java

--- 0707-design-linked-list/0707-design-linked-list.java
+++ 0707-design-linked-list/0707-design-linked-list.java
@@ -66,12 +66,27 @@ class MyLinkedList {
     }
 }
 
-/**
- * Your MyLinkedList object will be instantiated and called as such:
- * MyLinkedList obj = new MyLinkedList();
- * int param_1 = obj.get(index);
- * obj.addAtHead(val);
- * obj.addAtTail(val);
- * obj.addAtIndex(index,val);
- * obj.deleteAtIndex(index);
+/*
+class MyLinkedList {
+    private ArrayList<Integer> list;
+    public MyLinkedList() {
+         list = new ArrayList<>();
+    }
+    public int get(int index) {
+        if(index < list.size()) return list.get(index);
+        else return -1;
+    }
+    public void addAtHead(int val) {
+        list.add(0, val);
+    }
+    public void addAtTail(int val) {
+        list.add(val);
+    }
+    public void addAtIndex(int index, int val) {
+        if(index <= list.size()) list.add(index,val);
+    }
+    public void deleteAtIndex(int index) {
+         if(index < list.size()) list.remove(index);
+    }
+}
  */
