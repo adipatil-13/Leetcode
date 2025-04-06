26de08914d885489580407fb357511b5d42ae156
Aditya Patil <121687565+adipatil-13@users.noreply.github.com>
2025-04-07 00:14:09 +0530

    Time: 11 ms (65.42%), Space: 45.1 MB (43.68%) - LeetHub

--- 0424-longest-repeating-character-replacement/0424-longest-repeating-character-replacement.java
+++ 0424-longest-repeating-character-replacement/0424-longest-repeating-character-replacement.java
@@ -1,5 +1,28 @@
 class Solution {
     public int characterReplacement(String s, int k) {
+        if (s.isEmpty() || k > s.length()) return s.length();
+
+        int[] freq = new int[26];
+        int r = 0, l = 0, res = 0, maxFreq = 0;
+
+        while (r < s.length()) {
+            char c = s.charAt(r);
+            freq[c - 'A']++;
+            maxFreq = Math.max(maxFreq, freq[c - 'A']);
+
+            if ((r - l + 1) - maxFreq > k) {
+                freq[s.charAt(l) - 'A']--;
+                l++;
+            }
+            res = Math.max(res, r - l + 1);
+            r++;
+        }
+        return res;
+    }
+}
+/*
+class Solution {
+    public int characterReplacement(String s, int k) {
         Map<Character, Integer> map = new HashMap<>();
         int left = 0, right = 0, maxFreq = 0, res = 0;
 
@@ -17,3 +40,4 @@ class Solution {
         return res;
     }
 }
+*/
