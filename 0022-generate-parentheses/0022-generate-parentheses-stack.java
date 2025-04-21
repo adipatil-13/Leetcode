62902bf825108ab2ea19bc24c4d41f2d750b1864
Aditya Patil <121687565+adipatil-13@users.noreply.github.com>
2025-04-21 23:02:43 +0530

    Update 0022-generate-parentheses-stack.java

--- 0022-generate-parentheses/0022-generate-parentheses-stack.java
+++ 0022-generate-parentheses/0022-generate-parentheses-stack.java
@@ -1,6 +1,36 @@
 class Solution {
     public List<String> generateParenthesis(int n) {
         List<String> res = new ArrayList<>();
+        Stack<Character> stack = new Stack<>();
+        backtrack(0, 0, n, res, stack);
+        return res;
+    }
+    public void backtrack(int open, int closed, int n, List<String> res, Stack<Character> stack) {
+        if (open == closed && open == n) {
+            StringBuilder sb = new StringBuilder();
+
+            for (char c : stack) sb.append(c);
+
+            res.add(sb.toString());
+            return;
+        }
+
+        if (open < n) {
+            stack.push('(');
+            backtrack(open + 1, closed, n, res, stack);
+            stack.pop(); 
+        }
+        if (closed < open) {
+            stack.push(')');
+            backtrack(open, closed + 1, n, res, stack);
+            stack.pop(); 
+        }
+    }
+}
+/*
+class Solution {
+    public List<String> generateParenthesis(int n) {
+        List<String> res = new ArrayList<>();
         StringBuilder stack = new StringBuilder();
         backtrack(0, 0, n, res, stack);
         return res;
@@ -23,3 +53,4 @@ class Solution {
         }
     }
 }
+*/
