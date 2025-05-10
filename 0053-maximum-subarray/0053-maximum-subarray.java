7c7441304eaf3d54dfc137ffb87f172ed134ded9
Aditya Patil <121687565+adipatil-13@users.noreply.github.com>
2025-05-10 13:12:37 +0530

    Time: 1 ms (99.6%), Space: 56.9 MB (81.28%) - LeetHub

--- 0053-maximum-subarray/0053-maximum-subarray.java
+++ 0053-maximum-subarray/0053-maximum-subarray.java
@@ -1,5 +1,6 @@
 class Solution {
     public int maxSubArray(int[] nums) {
+        //Kadane Algorithm, O(n), O(1)
         int res = Integer.MIN_VALUE, sum = 0;
 
         for(int i = 0; i < nums.length; i++){
