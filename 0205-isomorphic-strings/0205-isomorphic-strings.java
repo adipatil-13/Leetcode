e3ec1640e8bcfc6abd3fc4c0e6f480366d1517ec
Aditya Patil <121687565+adipatil-13@users.noreply.github.com>
2025-05-12 16:05:41 +0530

    Time: 17 ms (38.25%), Space: 43 MB (11.8%) - LeetHub

--- 0205-isomorphic-strings/0205-isomorphic-strings.java
+++ 0205-isomorphic-strings/0205-isomorphic-strings.java
@@ -1,43 +1,18 @@
-// 13ms
-
 class Solution {
     public boolean isIsomorphic(String s, String t) {
-        Map<Character, Character> map = new HashMap<>();
-        if (s.length() != t.length()) return false;
+        HashMap<Character, Integer> smap = new HashMap<>();
+        HashMap<Character, Integer> tmap = new HashMap<>();
 
         for (int i = 0; i < s.length(); i++) {
-            char sChar = s.charAt(i);
-            char tChar = t.charAt(i);
-
-            if (map.containsKey(sChar)) {
-                if (map.get(sChar) != tChar) return false;
-            } else if (map.containsValue(tChar)) {
+            if (!smap.containsKey(s.charAt(i))) {
+                smap.put(s.charAt(i), i);
+            }
+            if (!tmap.containsKey(t.charAt(i))) {
+                tmap.put(t.charAt(i), i);
+            }
+            if (!smap.get(s.charAt(i)).equals(tmap.get(t.charAt(i)))) {
                 return false;
             }
-
-            map.put(sChar, tChar);
-        }
-        return true;
-    }
-}
-
-// 8 ms
-
-class Solution {
-    public boolean isIsomorphic(String s, String t) {
-        int[] charS = new int[200];
-        int[] charT = new int[200]; 
-        
-        if(s.length() != t.length()) {
-            return false;
-        }
-        
-        for(int i = 0; i < s.length(); i++) {
-            if(charS[s.charAt(i)] != charT[t.charAt(i)]) {
-                return false; 
-            }
-            charS[s.charAt(i)] = i + 1;
-            charT[t.charAt(i)] = i + 1; 
         }
         return true;
     }
