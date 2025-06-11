705c164bf3de5c9a87e70f0c1e109b4cacba0291
Aditya Patil <121687565+adipatil-13@users.noreply.github.com>
2025-06-12 00:12:47 +0530

    Time: 7 ms (87.21%), Space: 57 MB (50.97%) - LeetHub

--- 1020-number-of-enclaves/1020-number-of-enclaves.java
+++ 1020-number-of-enclaves/1020-number-of-enclaves.java
@@ -1,54 +1,38 @@
 class Solution {
     public int numEnclaves(int[][] grid) {
-        Queue<Pair> queue = new LinkedList<>();
-        int n = grid.length;
-        int m = grid[0].length;
+        int m = grid.length;
+        int n = grid[0].length;
 
-        int[][] visited = new int[n][m];
-
-        for (int i = 0; i < n; i++) {
-            for (int j = 0; j < m; j++) {
-                if (i == 0 || j == 0 || i == n - 1 || j == m - 1) {
-                    if (grid[i][j] == 1) {
-                        queue.add(new Pair(i, j));
-                        visited[i][j] = 1;
-                    }
-                }
-            }
+        // Step 1: Flood fill from boundary land cells
+        for (int i = 0; i < m; i++) {
+            dfs(grid, i, 0);
+            dfs(grid, i, n - 1);
+        }
+        for (int j = 0; j < n; j++) {
+            dfs(grid, 0, j);
+            dfs(grid, m - 1, j);
         }
 
-        int[] delrow = {-1, 0, +1, 0};
-        int[] delcol = {0, +1, 0, -1};
-
-        while (!queue.isEmpty()) {
-            int row = queue.peek().first;
-            int col = queue.peek().second;
-            queue.remove();
-
-            for (int i = 0; i < 4; i++) {
-                int newRow = row + delrow[i];
-                int newcol = col + delcol[i];
-
-                if (newRow >= 0 && newRow < n && newcol >= 0 && newRow < n && 
-                        visited[newRow][newcol] == 0 && grid[newRow][newcol] == 1) {
-                    queue.add(new Pair(newRow, newcol));
-                    visited[newRow][newcol] = 1;
-                }
-            }
-        }
+        // Step 2: Count remaining 1s
         int count = 0;
-        for (int i = 0; i < n; i++) {
-            for (int j = 0; j < m; j++) {
-                if (grid[i][j] == 1 && visited[i][j] == 0) count++;
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                if (grid[i][j] == 1) count++;
             }
         }
+
         return count;
     }
-}
-public class Pair {
-    int first, second;
-    Pair (int _first, int _second) {
-        this.first = _first;
-        this.second = _second;
+
+    private void dfs(int[][] grid, int i, int j) {
+        if (i < 0 || j < 0 || i >= grid.length || 
+                j >= grid[0].length || grid[i][j] != 1) 
+            return;
+        
+        grid[i][j] = 0;
+        dfs(grid, i + 1, j);
+        dfs(grid, i - 1, j);
+        dfs(grid, i, j + 1);
+        dfs(grid, i, j - 1);
     }
 }
