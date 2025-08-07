7e6e767321eb19b82104ba04c8ea1510425829d1
Aditya Patil <121687565+adipatil-13@users.noreply.github.com>
2025-08-08 04:27:10 +0530

    Time: 1 ms (97.42%), Space: 44.5 MB (53.87%) - LeetHub

---
+++ 3396-minimum-number-of-operations-to-make-elements-in-array-distinct/3396-minimum-number-of-operations-to-make-elements-in-array-distinct.java
@@ -0,0 +1,10 @@
+class Solution {
+    public int minimumOperations(int[] nums) {
+        int[] freq = new int[101];
+        for (int i = nums.length - 1; i >= 0; --i) {
+            if (++freq[nums[i]] > 1) 
+                return (i + 3) / 3;
+        }
+        return 0;
+    }
+}
