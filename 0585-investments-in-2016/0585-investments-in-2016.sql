6f8985f957bca1efd68864246ae53ebd29b4fb5a
Aditya Patil <121687565+adipatil-13@users.noreply.github.com>
2025-09-22 02:55:06 +0530

    Time: 473 ms (97.18%), Space: 0B (100%) - LeetHub

--- 0585-investments-in-2016/0585-investments-in-2016.sql
+++ 0585-investments-in-2016/0585-investments-in-2016.sql
@@ -1,12 +1,9 @@
-SELECT ROUND(SUM(tiv_2016), 2) AS tiv_2016
-FROM Insurance
-WHERE tiv_2015 IN (
-    SELECT tiv_2015 FROM Insurance
-    GROUP BY tiv_2015
-    HAVING COUNT(*) > 1
-)
-AND (lat, lon) IN (
-    SELECT lat, lon FROM Insurance
-    GROUP BY lat, lon
-    HAVING COUNT(*) = 1
-);
+SELECT  
+    ROUND(SUM(tiv_2016), 2) AS tiv_2016
+FROM (
+    SELECT * 
+    , COUNT(*) OVER(PARTITION by tiv_2015) tiv_2015_cnt
+    , COUNT(*) OVER(PARTITION by lat,lon) city_cnt
+    FROM Insurance
+) t
+WHERE city_cnt = 1 AND tiv_2015_cnt > 1
