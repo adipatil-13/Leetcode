a9f7ed1b1bd072ccd5bfeab21afab6c8b1a6c062
Aditya Patil <121687565+adipatil-13@users.noreply.github.com>
2025-09-30 03:26:28 +0530

    Time: 1041 ms (96.67%), Space: 0B (100%) - LeetHub

--- 1158-market-analysis-i/1158-market-analysis-i.sql
+++ 1158-market-analysis-i/1158-market-analysis-i.sql
@@ -1,6 +1,9 @@
-SELECT u.user_id AS buyer_id , u.join_date, IFNULL(COUNT(o.order_id), 0) AS orders_in_2019
+SELECT u.user_id as buyer_id, u.join_date, COUNT(o.order_id) AS orders_in_2019
 FROM Users u
-LEFT JOIN Orders o
-ON u.user_id = o.buyer_id
-AND YEAR(order_date) = '2019'
-GROUP BY u.user_id;
+LEFT JOIN (
+    SELECT order_id, order_date, buyer_id
+    FROM Orders
+    WHERE order_date >= '2019-01-01' AND order_date < '2020-01-01'
+) o
+ON u.user_id = o.buyer_id 
+GROUP BY u.user_id, u.join_date;
