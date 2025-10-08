2c0e7deedad1f60ed5e167e1c506ce03bf75996e
Aditya Patil <121687565+adipatil-13@users.noreply.github.com>
2025-10-09 00:27:15 +0530

    Time: 1 ms (99.75%), Space: 47.7 MB (57.36%) - LeetHub

--- 0064-minimum-path-sum/0064-minimum-path-sum.java
+++ 0064-minimum-path-sum/0064-minimum-path-sum.java
@@ -5,22 +5,22 @@ class Solution {
         int[][] dp = new int[m][n];
 
         for (int i = 0; i < m; i++) {
-            for (int j = 0; j < n; j++) {
-                if (i == 0 && j == 0) dp[i][j] = grid[i][j];
-
-                else {
-                    int up = grid[i][j];
-                    if (i > 0) up += dp[i - 1][j];
-                    else up += 1e9;
-
-                    int left = grid[i][j];
-                    if (j > 0) left += dp[i][j - 1];
-                    else left += 1e9;
-
-                    dp[i][j] = Math.min(up, left);
-                }
-            }
+            Arrays.fill(dp[i], -1);
         }
-        return dp[m - 1][n - 1];
+        return helper(0, 0, grid, dp);
+    }
+    private int helper(int i, int j, int[][] grid, int[][] dp) {
+        int m = grid.length, n = grid[0].length;
+        if (i == m - 1 && j == n - 1) return grid[i][j];
+
+        if (i > m - 1 || j > n - 1) return Integer.MAX_VALUE;
+
+        if (dp[i][j] != -1) return dp[i][j];
+
+        int down = helper(i + 1, j, grid, dp);
+        int right = helper(i, j + 1, grid, dp);
+
+        dp[i][j] = grid[i][j] + Math.min(down, right);
+        return dp[i][j];
     }
 }
