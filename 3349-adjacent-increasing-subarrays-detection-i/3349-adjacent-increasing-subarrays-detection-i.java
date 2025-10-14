ad5ca3c0dfae0b7428916f741304732eb8bf5a69
Aditya Patil <121687565+adipatil-13@users.noreply.github.com>
2025-10-14 18:13:52 +0530

    Time: 1 ms (100%), Space: 44.8 MB (25%) - LeetHub

--- 3349-adjacent-increasing-subarrays-detection-i/3349-adjacent-increasing-subarrays-detection-i.java
+++ 3349-adjacent-increasing-subarrays-detection-i/3349-adjacent-increasing-subarrays-detection-i.java
@@ -1,32 +1,21 @@
 class Solution {
     public boolean hasIncreasingSubarrays(List<Integer> nums, int k) {
-        List<Integer> list = new ArrayList<>();
-        int cnt = 1;
-        if (nums.size() >= 2 * k) {
-            for (int i = 1; i < nums.size(); i++) {
-                if (nums.get(i) > nums.get(i - 1)) {
-                    cnt++;
-                    if (cnt >= 2 * k) return true;
-                } else {
-                    cnt = 1;
-                }
+        int n = nums.size();
+        
+        for (int i = 0; i + 2 * k <= n; i++) {
+            if (isStrictlyInc(nums, i, i + k - 1) &&
+                isStrictlyInc(nums, i + k, i + 2 * k - 1)) {
+                return true;
             }
-        }
-        for (int i = 0; i <= nums.size() - k; i++) {
-            boolean isInc = true;
-
-            for (int j = i; j < i + k - 1; j++) {
-                if (nums.get(j) >= nums.get(j + 1)) {
-                    isInc = false;
-                    break;
-                }
-            }
-            if (isInc) list.add(i);
-        }
-
-        for (int i = 0; i < list.size() - 1; i++) {
-            if (list.get(i + 1) - list.get(i) == k) return true;
         }
         return false;
+    }
+    
+    private boolean isStrictlyInc(List<Integer> nums, int start, int end) {
+        for (int i = start; i < end; i++) {
+            if (nums.get(i) >= nums.get(i + 1)) 
+                return false; 
+        }
+        return true;
     }
 }
