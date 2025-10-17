0eae1869c8fa0aee0805bdfab753f775b7377328
Aditya Patil <121687565+adipatil-13@users.noreply.github.com>
2025-10-18 01:17:24 +0530

    Time: 3 ms (91.55%), Space: 50.3 MB (82.82%) - LeetHub

--- 1289-minimum-falling-path-sum-ii/1289-minimum-falling-path-sum-ii.java
+++ 1289-minimum-falling-path-sum-ii/1289-minimum-falling-path-sum-ii.java
@@ -1,29 +1,38 @@
 class Solution {
     public int minFallingPathSum(int[][] grid) {
         int n = grid.length;
-        int[][] dp = new int[n][n];
+
+        int min1 = Integer.MAX_VALUE, min2 = Integer.MAX_VALUE;
+        int min1Idx = -1;
 
         for (int i = 0; i < n; i++) {
-            Arrays.fill(dp[i], Integer.MAX_VALUE);
-        }
-        int min = Integer.MAX_VALUE;
-        for (int i = 0; i < n; i++) {
-            min = Math.min(min, dfs(0, i, grid, dp));
-        }
-        return min;
-    }
-    private int dfs(int i, int j, int[][] grid, int[][] dp) {
-        int n = grid.length;
-        if (i == n - 1) return grid[i][j];
+            int newMin1 = Integer.MAX_VALUE, newMin2 = Integer.MAX_VALUE;
+            int newMin1Idx = -1;
 
-        if (dp[i][j] != Integer.MAX_VALUE) return dp[i][j];
+            for (int j = 0; j < n; j++) {
+                if (i > 0) {
+                    if (j == min1Idx) {
+                        grid[i][j] += min2;
+                    } else {
+                        grid[i][j] += min1;
+                    }
+                }
 
-        int min = Integer.MAX_VALUE;
+                int val = grid[i][j];
 
-        for (int k = 0; k < n; k++) {
-            if (k == j) continue;
-            min = Math.min(min, dfs(i + 1, k, grid, dp));
+                if (val < newMin1) {
+                    newMin2 = newMin1;
+                    newMin1 = val;
+                    newMin1Idx = j;
+                } else if (val < newMin2) {
+                    newMin2 = val;
+                }
+            }
+
+            min1 = newMin1;
+            min2 = newMin2;
+            min1Idx = newMin1Idx;
         }
-        return dp[i][j] = grid[i][j] + min;
+        return min1;
     }
 }
