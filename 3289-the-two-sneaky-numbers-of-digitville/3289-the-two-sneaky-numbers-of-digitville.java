d88cd2024092ee955f22f899f15c8a52b697c485
Aditya Patil <121687565+adipatil-13@users.noreply.github.com>
2025-10-31 19:08:47 +0530

    Time: 1 ms (100%), Space: 45.6 MB (7.71%) - LeetHub

--- 3289-the-two-sneaky-numbers-of-digitville/3289-the-two-sneaky-numbers-of-digitville.java
+++ 3289-the-two-sneaky-numbers-of-digitville/3289-the-two-sneaky-numbers-of-digitville.java
@@ -1,14 +1,31 @@
 class Solution {
     public int[] getSneakyNumbers(int[] nums) {
-        int[] res = new int[2];
-        int k = 0;
-        Map<Integer, Integer> freq = new HashMap<>();
+        int xor = 0;
+        int n = nums.length - 2;
+
+        for (int num : nums) 
+            xor ^= num;
+
+        for (int i = 0; i < n; i++) 
+            xor ^= i;
+
+        int diffBit = xor & -xor;
+
+        int a = 0, b = 0;
 
         for (int num : nums) {
-            int x = freq.getOrDefault(num, 0) + 1;
-            freq.put(num, x);
-            if (x == 2) res[k++] = num;
+            if ((num & diffBit) == 0) 
+                a ^= num;
+            else 
+                b ^= num;
         }
-        return res;
+
+        for (int i = 0; i < n; i++) {
+            if ((i & diffBit) == 0) 
+                a ^= i;
+            else 
+                b ^= i;
+        }
+        return new int[]{a, b};
     }
 }
