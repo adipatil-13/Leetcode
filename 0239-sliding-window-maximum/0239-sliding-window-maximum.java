10c3a13321e21113cb254b185403ecb627706b02
Aditya Patil <121687565+adipatil-13@users.noreply.github.com>
2025-11-01 02:47:47 +0530

    Time: 30 ms (77.73%), Space: 152.6 MB (5.33%) - LeetHub

---
+++ 0239-sliding-window-maximum/0239-sliding-window-maximum.java
@@ -0,0 +1,24 @@
+class Solution {
+    public int[] maxSlidingWindow(int[] nums, int k) {
+        int n = nums.length;
+        if (n == 0 || k == 0) return new int[0];
+
+        int[] res = new int[n - k + 1];
+
+        LinkedList<Integer> dq = new LinkedList<>();
+
+        for (int i = 0; i < n; i++) {
+            if (!dq.isEmpty() && dq.peek() < i - k + 1) {
+                dq.poll();
+            }
+            while (!dq.isEmpty() && nums[i] >= nums[dq.peekLast()]) {
+                dq.pollLast();
+            }
+            dq.offer(i);
+            if (i - k + 1 >= 0) {
+                res[i - k + 1] = nums[dq.peek()];
+            }
+        }
+        return res;
+    }
+}
