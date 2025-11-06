9809588a2825cb4a5eeacb6a3d2b5bf951c58f25
Aditya Patil <121687565+adipatil-13@users.noreply.github.com>
2025-11-06 20:45:50 +0530

    Time: 10 ms (38.38%), Space: 47.3 MB (5%) - LeetHub

--- 0506-relative-ranks/0506-relative-ranks.java
+++ 0506-relative-ranks/0506-relative-ranks.java
@@ -1,25 +1,32 @@
 class Solution {
     public String[] findRelativeRanks(int[] score) {
         int n = score.length;
-        int[][] pair = new int[n][2];
-
-        for (int i = 0; i < n; i++) {
-            pair[i] = new int[] {i, score[i]};
+        
+        PriorityQueue<Pair<Integer, Integer>> pq = new PriorityQueue<>((p1, p2) -> (p2.getValue() - p1.getValue()));
+        
+        for (int i = 0 ; i < n ; i++) {
+            pq.add(new Pair(i, score[i]));
         }
-        Arrays.sort(pair, (a, b) -> b[1] - a[1]);
+        
         String[] res = new String[n];
-
-        for (int i = 0; i < n; i++) {
-            if (i == 0) {
-                res[pair[i][0]] = "Gold Medal";
-            } else if (i == 1) {
-                res[pair[i][0]] = "Silver Medal";
-            } else if (i == 2) {
-                res[pair[i][0]] = "Bronze Medal";
+        int place = 1;
+        
+        while (!pq.isEmpty()) {
+            Pair<Integer, Integer> top = pq.poll();
+            int curr = top.getKey();
+            
+            if (place == 1) {
+                res[curr] = "Gold Medal";
+            } else if (place == 2) {
+                res[curr] = "Silver Medal";
+            } else if (place == 3) {
+                res[curr] = "Bronze Medal";
             } else {
-                res[pair[i][0]] = String.valueOf(i + 1);
+                res[curr] = String.valueOf(place);
             }
+            place++;
         }
+        
         return res;
     }
 }
