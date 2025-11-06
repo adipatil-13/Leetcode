e853b5a66da9afac77854cdb9f2ad5a6ee6d327a
Aditya Patil <121687565+adipatil-13@users.noreply.github.com>
2025-11-06 20:31:35 +0530

    Time: 37 ms (99.93%), Space: 52 MB (5.11%) - LeetHub

--- 0384-shuffle-an-array/0384-shuffle-an-array.java
+++ 0384-shuffle-an-array/0384-shuffle-an-array.java
@@ -1,35 +1,30 @@
 class Solution {
-    private int[] nums;
+    private final Integer[] nums;
+    private final Integer[] res;
+    private final Random rand = new Random();
 
     public Solution(int[] nums) {
-        this.nums = nums;
+        this.nums = new Integer[nums.length];
+        res = new Integer[nums.length];
+
+        for (int i = 0; i < nums.length; i++) {
+            this.nums[i] = nums[i];
+            res[i] = nums[i];
+        }
     }
     
-    public int[] reset() {
+    public Integer[] reset() {
         return nums;
     }
     
-    public int[] shuffle() {
-        int[] res = new int[nums.length];
-		res = nums.clone();
-		
-		for(int i = 0; i < nums.length; i++){
-			int rand = (int)(Math.random() * (i + 1));
-			swap(res, i, rand);
-		}
-		return res;
+    public Integer[] shuffle() {
+        for (int i = nums.length - 1; i > 0; i--) {
+            int j = rand.nextInt(i + 1); 
+
+            Integer temp = res[i];
+            res[i] = res[j];
+            res[j] = temp;
+        }
+        return res;
     }
-
-    public void swap(int[] nums, int i, int j){
-		int temp = nums[i];
-		nums[i] = nums[j];
-		nums[j] = temp;
-	}
 }
-
-/**
- * Your Solution object will be instantiated and called as such:
- * Solution obj = new Solution(nums);
- * int[] param_1 = obj.reset();
- * int[] param_2 = obj.shuffle();
- */
