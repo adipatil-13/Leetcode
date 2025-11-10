09260d0f52116e8e2900366355fadaed46a517d5
Aditya Patil <121687565+adipatil-13@users.noreply.github.com>
2025-11-10 19:54:49 +0530

    Time: 12 ms (96.48%), Space: 137.7 MB (5.63%) - LeetHub

--- 3542-minimum-operations-to-convert-all-elements-to-zero/3542-minimum-operations-to-convert-all-elements-to-zero.java
+++ 3542-minimum-operations-to-convert-all-elements-to-zero/3542-minimum-operations-to-convert-all-elements-to-zero.java
@@ -1,18 +1,33 @@
 class Solution {
     public int minOperations(int[] nums) {
-        Deque<Integer> dq = new ArrayDeque<>();
-        dq.push(0);
-        int res = 0;
+        int n = nums.length;
+        int[] temp = new int[n + 1];
+        int top = 0, res = 0;
 
-        for (int num : nums) {
-            while (!dq.isEmpty() && dq.peek() > num) {
-                dq.pop();
-            }
-            if (dq.isEmpty() || num > dq.peek()) {
+        for (int i = 0; i < n; i++) {
+            while (temp[top] > nums[i]) {
+                top--;
                 res++;
-                dq.push(num);
             }
+            if (temp[top] != nums[i])
+                temp[++top] = nums[i];
         }
-        return res;
+        return res + top;
+
+
+        // Deque<Integer> dq = new ArrayDeque<>();
+        // dq.push(0);
+        // int res = 0;
+
+        // for (int num : nums) {
+        //     while (!dq.isEmpty() && dq.peek() > num) {
+        //         dq.pop();
+        //     }
+        //     if (dq.isEmpty() || num > dq.peek()) {
+        //         res++;
+        //         dq.push(num);
+        //     }
+        // }
+        // return res;
     }
 }
